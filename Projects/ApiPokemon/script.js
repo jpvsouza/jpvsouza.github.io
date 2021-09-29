877cc51d274c98475555d3ca1078b811e9b6fdcf
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-09-29 14:38:24 -0300

    pegando dados do bulbasaur

--- Projects/ApiPokemon/script.js
+++ Projects/ApiPokemon/script.js
@@ -20,10 +20,38 @@ const pegaLista = (enderecoAtual) => {
     resultado.results.forEach((itemApi) => {
       const itemLista = document.createElement('li');
       itemLista.innerHTML = itemApi.name;
+      itemLista.addEventListener('click', () => {
+        if (itemApi.name === 'bulbasaur') {
+          pegaPokemon(itemApi.url);
+        }
+      });
       listaOrdenada.appendChild(itemLista);
     });
   })
   .catch((erro) => console.log(erro));
+};
+
+const pegaPokemon = (url) => {
+  const resultado = verificaFetch(url)
+  .then((resultado) => {
+    atualizaFormulario(resultado);
+  })
+};
+
+const atualizaFormulario = (pokemon) => {
+  const tagNome = document.getElementById('nome');
+  const tagNumero = document.getElementById('id');
+  const tagTipo1 = document.getElementById('tipo1');
+  const tagTipo2 = document.getElementById('tipo2');
+  const tagPeso = document.getElementById('peso');
+  const tagFoto = document.getElementById('foto');
+
+  tagNome.innerHTML = pokemon.name;
+  tagNumero.innerHTML = pokemon.id;
+  tagTipo1.innerHTML = pokemon.types[0].type.name;
+  tagTipo2.innerHTML = pokemon.types[1].type.name;
+  tagPeso.innerHTML = pokemon.weight;
+  tagFoto.src = pokemon.sprites.other.dream_world.front_default;
 };
 
 pegaLista(enderecoApi);
